c28c375ea0b60498f075cd48e1303bf87a0d48bd
andrés <olivera.h.andres@gmail.com>
2023-08-24 23:54:16 -0300

    CRUD episodio I

---
+++ src/controllers/mainController.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		// Do the magic
+		return res.render('index', {
+			productsVisited : products.filter(product => product.category === 'visited'),
+			productsInSale : products.filter(product => product.category === 'in-sale'),
+			toThousand
+		})
+	},
+	search: (req, res) => {
+		// Do the magic
+		const results = products.filter(product => product.name.toLowerCase().includes(req.query.keywords.toLowerCase()));
+		
+		return res.render('results', {
+			results,
+			toThousand,
+			keywords : req.query.keywords
+		});
+	},
+};
+
+module.exports = controller;
